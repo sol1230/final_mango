ec2249055bc20b008a4695ba8f678be2f7ea5a54
sol1230 <solllll1230@gmail.com>
2022-12-27 15:23:39 +0900

    first commit

---
+++ src/test/java/com/mango/final_mango/FinalMangoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mango.final_mango;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalMangoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
