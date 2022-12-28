90a524f23cf66e2d424f7b8a7402751ead4111ce
sohiekim65 <sohiekim65@gmail.com>
2022-12-28 15:49:42 +0900

    add @ServletComponentScan

--- src/main/java/com/mango/final_mango/FinalMangoApplication.java
+++ src/main/java/com/mango/final_mango/FinalMangoApplication.java
@@ -2,7 +2,9 @@ package com.mango.final_mango;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan
 @SpringBootApplication
 public class FinalMangoApplication {
 
