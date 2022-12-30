a9ffb4dcf43657d6ed5aca8367bda8c4916f116a
ParkChanhee1130 <kkazza1130@gmail.com>
2022-12-30 14:05:30 +0900

    12.30 중간

--- src/main/java/com/mango/final_mango/servlets/membership.java
+++ src/main/java/com/mango/final_mango/servlets/membership.java
@@ -1,6 +1,7 @@
 package com.mango.final_mango.servlets;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
@@ -14,6 +15,16 @@ public class membership extends HttpServlet{
  
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        HashMap<String,Object> userData = new HashMap<>();
+
+        userData.put("USER_ID", request.getParameter("USER_id"));
+        userData.put("PASSWORD", request.getParameter("PASSWORD"));
+        userData.put("NAME", request.getParameter("NAME"));
+        userData.put("BIRTH_DATE", request.getParameter("BIRTH_DATE"));
+        userData.put("PHONE", request.getParameter("PHONE"));
+
+
         RequestDispatcher requestDispatcher = request.getRequestDispatcher("/membership.jsp");
         requestDispatcher.forward(request, response);
     }
