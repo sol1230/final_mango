014dce614397c5cc64a5619034e9668104a2f5c5
sol1230 <solllll1230@gmail.com>
2022-12-30 14:33:08 +0900

    suevey

--- src/main/java/com/mango/final_mango/servlets/SurveyServlets.java
+++ src/main/java/com/mango/final_mango/servlets/SurveyServlets.java
@@ -12,7 +12,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-@WebServlet(urlPatterns = "/survey/SurveyServlets")
+@WebServlet(urlPatterns = "/survey/surveyServelts")
 public class SurveyServlets extends HttpServlet {
 
   @Override
@@ -21,32 +21,38 @@ public class SurveyServlets extends HttpServlet {
     HttpServletResponse response
   )
     throws ServletException, IOException {
-    // input type
-    // response.setContentType("text/html;charset=UTF-8");
-    String questions_uid = request.getParameter("QUESTION_UID");
+    response.setContentType("text/html;charset=UTF-8");
 
-    // biz with DB and Class
+    // DB 불러오기
     SurveyWithDB surveyWithDB = new SurveyWithDB();
 
+    ArrayList<HashMap> question_list = null;
     HashMap<String, Object> question = null;
-    ArrayList<HashMap> answers = null;
+    ArrayList<HashMap> answers_list = null;
+    HashMap<String, Object> answers = null;
 
-    // display
     try {
-      question = surveyWithDB.getQuestion(questions_uid);
-      answers = surveyWithDB.getAnswer(questions_uid);
+      question_list = surveyWithDB.getQuestion();
+      answers_list = surveyWithDB.getAnswer();
+      // 질문 출력 확인용
+      for (int i = 0; i < question_list.size(); i++) {
+        question = question_list.get(i);
+        System.out.println(question.get("QUESTION_LIST"));
+        for (int j = 0; j < answers_list.size(); j++) {
+          answers = answers_list.get(j);
+          System.out.println(answers.get("ANSWER_LIST"));
+        }
+      }
     } catch (SQLException e) {
       e.printStackTrace();
     }
-
-    // output with html
-    request.setAttribute("question", question);
-    request.setAttribute("answers", answers);
-
-    // jsp
+    request.setAttribute("question_list", question_list);
+    request.setAttribute("answers_list", answers_list);
+    // jsp로 작동하도록 방향 트는 url 인스턴스화(=printWriter 대체용)
     RequestDispatcher requestDispatcher = request.getRequestDispatcher(
       "/jsp/survey.jsp"
     );
+    // forward하면서 request, response 모두 넘겨주는
     requestDispatcher.forward(request, response);
   }
 }
