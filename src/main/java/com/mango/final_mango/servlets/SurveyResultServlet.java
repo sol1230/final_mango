c6f0d4fa1e6a50aa97a43242b614e8263955ba23
sol1230 <solllll1230@gmail.com>
2023-01-02 23:36:29 +0900

    result

--- src/main/java/com/mango/final_mango/servlets/SurveyResultServlet.java
+++ src/main/java/com/mango/final_mango/servlets/SurveyResultServlet.java
@@ -1,6 +1,7 @@
 package com.mango.final_mango.servlets;
 
 import com.mango.final_mango.dao.SurveyResultWithDB;
+import com.mango.final_mango.dao.SurveyWithDB;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -9,6 +10,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -23,29 +25,57 @@ public class SurveyResultServlet extends HttpServlet {
   )
     throws ServletException, IOException {
     response.setContentType("text/html;charset=UTF-8");
-    // HttpSession httpSession = request.getSession();
+
     //DB
     SurveyResultWithDB surveyResultWithDB = new SurveyResultWithDB();
+    SurveyWithDB surveyWithDB = new SurveyWithDB();
 
-    ArrayList<HashMap> userSurvey = null;
+    PrintWriter printWriter = response.getWriter();
+
     ArrayList<HashMap> question_list = null;
-    String userName = "";
+    HashMap<String, Object> question = null;
+    ArrayList<HashMap> answers_list = null;
+    HashMap<String, Object> answers = null;
+
+    // user_id 넣기
+    HttpSession httpSession = request.getSession(false);
+
+    String user_id = "U1";
+    user_id = (String) httpSession.getAttribute("user_id");
 
     try {
-      userSurvey = surveyResultWithDB.getmySurvey();
-      question_list = surveyResultWithDB.getQuestion();
-      userName = surveyResultWithDB.getUserName();
+      boolean checkSurvey = surveyResultWithDB.checkUser(user_id);
+      String userName = surveyResultWithDB.getUserName(user_id);
+      if (checkSurvey) { // 설문 완료시
+        question_list = surveyWithDB.getQuestion();
+        answers_list = surveyWithDB.getAnswer();
+
+        //출력 확인
+        for (int i = 0; i < question_list.size(); i++) {
+          question = question_list.get(i);
+          System.out.println(question.get("QUESTION_LIST"));
+          for (int j = 0; j < answers_list.size(); j++) {
+            answers = answers_list.get(j);
+            System.out.println(answers.get("ANSWER_LIST"));
+          }
+        }
+
+        request.setAttribute("question_list", question_list);
+        request.setAttribute("answers_list", answers_list);
+        request.setAttribute("user_id", user_id);
+        request.setAttribute("userName", userName);
+
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher(
+          "/jsp/survey_result.jsp"
+        );
+        requestDispatcher.forward(request, response);
+      } else { // survey 미완료 시
+        printWriter.println(
+          "<script>alert('설문을 제출해주세요.'); history.back();</script>"
+        );
+      }
     } catch (SQLException e) {
       e.printStackTrace();
     }
-    request.setAttribute("userSurvey", userSurvey);
-    request.setAttribute("question_list", question_list);
-    request.setAttribute("userName", userName);
-
-    RequestDispatcher requestDispatcher = request.getRequestDispatcher(
-      "/jsp/survey_result.jsp"
-    );
-    // forward하면서 request, response 모두 넘겨주는
-    requestDispatcher.forward(request, response);
   }
 }
